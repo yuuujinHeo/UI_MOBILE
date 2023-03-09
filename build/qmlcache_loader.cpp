34fe8c8225fca5ac53d12ae0990b4a79bc4d8d48
yujinHeo <yjheo4@rainbow-robotics.com>
2023-03-10 08:55:24 +0900

    Efnke

--- build/qmlcache_loader.cpp
+++ build/qmlcache_loader.cpp
@@ -5,172 +5,76 @@
 static const unsigned char qt_resource_tree[] = {
 0,
 0,0,0,0,2,0,0,0,19,0,0,0,1,0,0,0,
-140,0,0,0,0,0,1,0,0,0,0,0,0,0,60,0,
-0,0,0,0,1,0,0,0,0,0,0,0,204,0,0,0,
-0,0,1,0,0,0,0,0,0,2,86,0,0,0,0,0,
-1,0,0,0,0,0,0,1,124,0,0,0,0,0,1,0,
-0,0,0,0,0,2,50,0,0,0,0,0,1,0,0,0,
-0,0,0,0,234,0,0,0,0,0,1,0,0,0,0,0,
-0,2,10,0,0,0,0,0,1,0,0,0,0,0,0,1,
-230,0,0,0,0,0,1,0,0,0,0,0,0,0,182,0,
-0,0,0,0,1,0,0,0,0,0,0,1,56,0,0,0,
-0,0,1,0,0,0,0,0,0,1,94,0,0,0,0,0,
-1,0,0,0,0,0,0,2,160,0,0,0,0,0,1,0,
-0,0,0,0,0,1,160,0,0,0,0,0,1,0,0,0,
-0,0,0,1,14,0,0,0,0,0,1,0,0,0,0,0,
-0,2,126,0,0,0,0,0,1,0,0,0,0,0,0,1,
-192,0,0,0,0,0,1,0,0,0,0,0,0,0,8,0,
-0,0,0,0,1,0,0,0,0,0,0,0,100,0,0,0,
+200,0,0,0,0,0,1,0,0,0,0,0,0,2,116,0,
+0,0,0,0,1,0,0,0,0,0,0,1,234,0,0,0,
+0,0,1,0,0,0,0,0,0,1,126,0,0,0,0,0,
+1,0,0,0,0,0,0,0,164,0,0,0,0,0,1,0,
+0,0,0,0,0,1,198,0,0,0,0,0,1,0,0,0,
+0,0,0,2,80,0,0,0,0,0,1,0,0,0,0,0,
+0,2,156,0,0,0,0,0,1,0,0,0,0,0,0,1,
+90,0,0,0,0,0,1,0,0,0,0,0,0,1,16,0,
+0,0,0,0,1,0,0,0,0,0,0,2,42,0,0,0,
+0,0,1,0,0,0,0,0,0,0,242,0,0,0,0,0,
+1,0,0,0,0,0,0,0,90,0,0,0,0,0,1,0,
+0,0,0,0,0,1,166,0,0,0,0,0,1,0,0,0,
+0,0,0,0,8,0,0,0,0,0,1,0,0,0,0,0,
+0,2,8,0,0,0,0,0,1,0,0,0,0,0,0,0,
+126,0,0,0,0,0,1,0,0,0,0,0,0,1,38,0,
+0,0,0,0,1,0,0,0,0,0,0,0,50,0,0,0,
 0,0,1,0,0,0,0};
 static const unsigned char qt_resource_names[] = {
 0,
-1,0,0,0,47,0,47,0,23,11,246,184,220,0,80,0,
-97,0,103,0,101,0,95,0,112,0,105,0,99,0,107,0,
-117,0,112,0,95,0,99,0,97,0,108,0,108,0,105,0,
-110,0,103,0,46,0,113,0,109,0,108,0,17,1,211,62,
-28,0,84,0,111,0,111,0,108,0,95,0,75,0,101,0,
+1,0,0,0,47,0,47,0,18,9,255,36,156,0,73,0,
+116,0,101,0,109,0,95,0,115,0,116,0,97,0,116,0,
+117,0,115,0,98,0,97,0,114,0,46,0,113,0,109,0,
+108,0,17,14,171,53,124,0,80,0,97,0,103,0,101,0,
+95,0,77,0,111,0,118,0,101,0,70,0,97,0,105,0,
+108,0,46,0,113,0,109,0,108,0,15,9,174,211,28,0,
+80,0,97,0,103,0,101,0,95,0,112,0,105,0,99,0,
+107,0,117,0,112,0,46,0,113,0,109,0,108,0,16,10,
+144,96,28,0,80,0,97,0,103,0,101,0,95,0,115,0,
+101,0,116,0,116,0,105,0,110,0,103,0,46,0,113,0,
+109,0,108,0,15,2,247,74,156,0,73,0,116,0,101,0,
+109,0,95,0,115,0,119,0,105,0,116,0,99,0,104,0,
+46,0,113,0,109,0,108,0,18,1,14,237,156,0,80,0,
+111,0,112,0,117,0,112,0,95,0,109,0,97,0,112,0,
+95,0,108,0,105,0,115,0,116,0,46,0,113,0,109,0,
+108,0,12,9,76,226,252,0,80,0,97,0,103,0,101,0,
+95,0,109,0,97,0,112,0,46,0,113,0,109,0,108,0,
+8,8,1,90,92,0,109,0,97,0,105,0,110,0,46,0,
+113,0,109,0,108,0,23,11,246,184,220,0,80,0,97,0,
+103,0,101,0,95,0,112,0,105,0,99,0,107,0,117,0,
+112,0,95,0,99,0,97,0,108,0,108,0,105,0,110,0,
+103,0,46,0,113,0,109,0,108,0,15,7,39,4,92,0,
+80,0,97,0,103,0,101,0,95,0,109,0,111,0,118,0,
+105,0,110,0,103,0,46,0,113,0,109,0,108,0,17,2,
+242,245,188,0,73,0,116,0,101,0,109,0,95,0,106,0,
+111,0,121,0,115,0,116,0,105,0,99,0,107,0,46,0,
+113,0,109,0,108,0,13,9,252,62,252,0,80,0,97,0,
+103,0,101,0,95,0,105,0,110,0,105,0,116,0,46,0,
+113,0,109,0,108,0,15,3,75,47,252,0,73,0,116,0,
+101,0,109,0,95,0,98,0,117,0,116,0,116,0,111,0,
+110,0,46,0,113,0,109,0,108,0,12,2,95,159,156,0,
+77,0,97,0,112,0,95,0,102,0,117,0,108,0,108,0,
+46,0,113,0,109,0,108,0,14,10,116,135,60,0,80,0,
+97,0,103,0,101,0,95,0,109,0,101,0,110,0,117,0,
+115,0,46,0,113,0,109,0,108,0,16,8,152,222,124,0,
+80,0,97,0,103,0,101,0,95,0,107,0,105,0,116,0,
+99,0,104,0,101,0,110,0,46,0,113,0,109,0,108,0,
+15,3,190,52,220,0,80,0,97,0,103,0,101,0,95,0,
+99,0,104,0,97,0,114,0,103,0,101,0,46,0,113,0,
+109,0,108,0,17,1,211,62,28,0,84,0,111,0,111,0,
+108,0,95,0,75,0,101,0,121,0,98,0,111,0,97,0,
+114,0,100,0,46,0,113,0,109,0,108,0,17,5,252,22,
+92,0,73,0,116,0,101,0,109,0,95,0,107,0,101,0,
 121,0,98,0,111,0,97,0,114,0,100,0,46,0,113,0,
-109,0,108,0,17,14,171,53,124,0,80,0,97,0,103,0,
-101,0,95,0,77,0,111,0,118,0,101,0,70,0,97,0,
-105,0,108,0,46,0,113,0,109,0,108,0,18,1,14,237,
-156,0,80,0,111,0,112,0,117,0,112,0,95,0,109,0,
-97,0,112,0,95,0,108,0,105,0,115,0,116,0,46,0,
-113,0,109,0,108,0,8,8,1,90,92,0,109,0,97,0,
-105,0,110,0,46,0,113,0,109,0,108,0,12,2,95,159,
-156,0,77,0,97,0,112,0,95,0,102,0,117,0,108,0,
-108,0,46,0,113,0,109,0,108,0,15,3,190,52,220,0,
-80,0,97,0,103,0,101,0,95,0,99,0,104,0,97,0,
-114,0,103,0,101,0,46,0,113,0,109,0,108,0,18,9,
-255,36,156,0,73,0,116,0,101,0,109,0,95,0,115,0,
-116,0,97,0,116,0,117,0,115,0,98,0,97,0,114,0,
-46,0,113,0,109,0,108,0,16,8,152,222,124,0,80,0,
-97,0,103,0,101,0,95,0,107,0,105,0,116,0,99,0,
-104,0,101,0,110,0,46,0,113,0,109,0,108,0,12,9,
-76,226,252,0,80,0,97,0,103,0,101,0,95,0,109,0,
-97,0,112,0,46,0,113,0,109,0,108,0,15,2,247,74,
-156,0,73,0,116,0,101,0,109,0,95,0,115,0,119,0,
-105,0,116,0,99,0,104,0,46,0,113,0,109,0,108,0,
-13,9,252,62,252,0,80,0,97,0,103,0,101,0,95,0,
-105,0,110,0,105,0,116,0,46,0,113,0,109,0,108,0,
-16,10,144,96,28,0,80,0,97,0,103,0,101,0,95,0,
-115,0,101,0,116,0,116,0,105,0,110,0,103,0,46,0,
-113,0,109,0,108,0,15,7,39,4,92,0,80,0,97,0,
-103,0,101,0,95,0,109,0,111,0,118,0,105,0,110,0,
-103,0,46,0,113,0,109,0,108,0,17,5,252,22,92,0,
-73,0,116,0,101,0,109,0,95,0,107,0,101,0,121,0,
-98,0,111,0,97,0,114,0,100,0,46,0,113,0,109,0,
-108,0,15,3,75,47,252,0,73,0,116,0,101,0,109,0,
-95,0,98,0,117,0,116,0,116,0,111,0,110,0,46,0,
-113,0,109,0,108,0,17,2,242,245,188,0,73,0,116,0,
-101,0,109,0,95,0,106,0,111,0,121,0,115,0,116,0,
-105,0,99,0,107,0,46,0,113,0,109,0,108,0,14,10,
-116,135,60,0,80,0,97,0,103,0,101,0,95,0,109,0,
-101,0,110,0,117,0,115,0,46,0,113,0,109,0,108,0,
-15,9,174,211,28,0,80,0,97,0,103,0,101,0,95,0,
-112,0,105,0,99,0,107,0,117,0,112,0,46,0,113,0,
 109,0,108};
 static const unsigned char qt_resource_empty_payout[] = { 0, 0, 0, 0, 0 };
 QT_BEGIN_NAMESPACE
 extern Q_CORE_EXPORT bool qRegisterResourceData(int, const unsigned char *, const unsigned char *, const unsigned char *);
 QT_END_NAMESPACE
 namespace QmlCacheGeneratedCode {
-namespace _0x5f__Page_pickup_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Page_menus_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Item_joystick_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Item_button_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
 namespace _0x5f__Item_keyboard_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Page_moving_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Page_setting_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Page_init_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Item_switch_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Page_map_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Page_kitchen_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Item_statusbar_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Page_charge_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Map_full_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__main_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Popup_map_list_qml { 
-    extern const unsigned char qmlData[];
-    const QQmlPrivate::CachedQmlUnit unit = {
-        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
-    };
-}
-namespace _0x5f__Page_MoveFail_qml { 
     extern const unsigned char qmlData[];
     const QQmlPrivate::CachedQmlUnit unit = {
         reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
@@ -182,7 +86,103 @@ namespace _0x5f__Tool_Keyboard_qml {
         reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
     };
 }
+namespace _0x5f__Page_charge_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Page_kitchen_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Page_menus_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Map_full_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Item_button_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Page_init_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Item_joystick_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Page_moving_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
 namespace _0x5f__Page_pickup_calling_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__main_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Page_map_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Popup_map_list_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Item_switch_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Page_setting_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Page_pickup_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Page_MoveFail_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__Item_statusbar_qml { 
     extern const unsigned char qmlData[];
     const QQmlPrivate::CachedQmlUnit unit = {
         reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
@@ -202,25 +202,25 @@ Q_GLOBAL_STATIC(Registry, unitRegistry)
 
 
 Registry::Registry() {
-        resourcePathToCachedUnit.insert(QStringLiteral("/Page_pickup.qml"), &QmlCacheGeneratedCode::_0x5f__Page_pickup_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Page_menus.qml"), &QmlCacheGeneratedCode::_0x5f__Page_menus_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Item_joystick.qml"), &QmlCacheGeneratedCode::_0x5f__Item_joystick_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Item_button.qml"), &QmlCacheGeneratedCode::_0x5f__Item_button_qml::unit);
         resourcePathToCachedUnit.insert(QStringLiteral("/Item_keyboard.qml"), &QmlCacheGeneratedCode::_0x5f__Item_keyboard_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Page_moving.qml"), &QmlCacheGeneratedCode::_0x5f__Page_moving_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Page_setting.qml"), &QmlCacheGeneratedCode::_0x5f__Page_setting_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Page_init.qml"), &QmlCacheGeneratedCode::_0x5f__Page_init_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Item_switch.qml"), &QmlCacheGeneratedCode::_0x5f__Item_switch_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Page_map.qml"), &QmlCacheGeneratedCode::_0x5f__Page_map_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Page_kitchen.qml"), &QmlCacheGeneratedCode::_0x5f__Page_kitchen_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Item_statusbar.qml"), &QmlCacheGeneratedCode::_0x5f__Item_statusbar_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Page_charge.qml"), &QmlCacheGeneratedCode::_0x5f__Page_charge_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Map_full.qml"), &QmlCacheGeneratedCode::_0x5f__Map_full_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/main.qml"), &QmlCacheGeneratedCode::_0x5f__main_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Popup_map_list.qml"), &QmlCacheGeneratedCode::_0x5f__Popup_map_list_qml::unit);
-        resourcePathToCachedUnit.insert(QStringLiteral("/Page_MoveFail.qml"), &QmlCacheGeneratedCode::_0x5f__Page_MoveFail_qml::unit);
         resourcePathToCachedUnit.insert(QStringLiteral("/Tool_Keyboard.qml"), &QmlCacheGeneratedCode::_0x5f__Tool_Keyboard_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Page_charge.qml"), &QmlCacheGeneratedCode::_0x5f__Page_charge_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Page_kitchen.qml"), &QmlCacheGeneratedCode::_0x5f__Page_kitchen_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Page_menus.qml"), &QmlCacheGeneratedCode::_0x5f__Page_menus_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Map_full.qml"), &QmlCacheGeneratedCode::_0x5f__Map_full_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Item_button.qml"), &QmlCacheGeneratedCode::_0x5f__Item_button_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Page_init.qml"), &QmlCacheGeneratedCode::_0x5f__Page_init_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Item_joystick.qml"), &QmlCacheGeneratedCode::_0x5f__Item_joystick_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Page_moving.qml"), &QmlCacheGeneratedCode::_0x5f__Page_moving_qml::unit);
         resourcePathToCachedUnit.insert(QStringLiteral("/Page_pickup_calling.qml"), &QmlCacheGeneratedCode::_0x5f__Page_pickup_calling_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/main.qml"), &QmlCacheGeneratedCode::_0x5f__main_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Page_map.qml"), &QmlCacheGeneratedCode::_0x5f__Page_map_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Popup_map_list.qml"), &QmlCacheGeneratedCode::_0x5f__Popup_map_list_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Item_switch.qml"), &QmlCacheGeneratedCode::_0x5f__Item_switch_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Page_setting.qml"), &QmlCacheGeneratedCode::_0x5f__Page_setting_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Page_pickup.qml"), &QmlCacheGeneratedCode::_0x5f__Page_pickup_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Page_MoveFail.qml"), &QmlCacheGeneratedCode::_0x5f__Page_MoveFail_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/Item_statusbar.qml"), &QmlCacheGeneratedCode::_0x5f__Item_statusbar_qml::unit);
     QQmlPrivate::RegisterQmlUnitCacheHook registration;
     registration.version = 0;
     registration.lookupCachedQmlUnit = &lookupCachedUnit;
